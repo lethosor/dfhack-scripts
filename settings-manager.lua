29d40b40041aa20d58544286ffa9689a8dc7abd6
lethosor <lethosor@gmail.com>
2014-06-17 23:08:06 -0400

    Working display

--- settings-manager.lua
+++ settings-manager.lua
@@ -1,9 +1,22 @@
 
 local gui = require "gui"
+local dialog = require 'gui.dialogs'
 local widgets = require "gui.widgets"
 
 SETTINGS = {
     init = {
+        {id = 'SOUND', type = 'bool', desc = 'Sound enabled'},
+        {id = 'VOLUME', type = 'int', desc = 'Volume'},
+        {id = 'INTRO', type = 'bool', desc = 'Display intro movies'},
+        {id = 'WINDOWED', type = 'select', desc = 'Start in windowed mode',
+            choices = {{'YES', 'Yes'}, {'NO', 'No'}, {'PROMPT', 'Prompt'}}
+        },
+        {id = 'WINDOWEDX', type = 'int', desc = 'Window X dimension (rows)'},
+        {id = 'WINDOWEDY', type = 'int', desc = 'Window Y dimension (columns)'},
+        {id = 'RESIZABLE', type = 'bool', desc = 'Allow resizing window'},
+        {id = 'FONT', type = 'string', desc = 'Font (windowed)', validate = function(s)
+            return dfhack.filesystem.exists('data/init/' .. s)
+        end},
         
     },
     d_init = {
@@ -11,10 +24,40 @@ SETTINGS = {
     }
 }
 
+BoolPrompt = defclass(BoolPrompt, dialog.MessageBox)
+
+BoolPrompt.ATTRS{
+    frame_style = gui.GREY_LINE_FRAME,
+    frame_inset = 1,
+    on_accept = DEFAULT_NIL,
+    on_cancel = DEFAULT_NIL,
+    on_close = DEFAULT_NIL,
+}
+function BoolPrompt:init(info)
+    self:addviews{
+        widgets.Label{
+            view_id = 'label',
+            text = {
+                {text='Yes', key='CUSTOM_Y', key_sep=':'},
+                NEWLINE,
+                {text='No', key='CUSTOM_N', key_sep=':'},
+            },
+            text_pen = info.text_pen,
+            frame = {l = 10, t = 10},
+            auto_height = true
+        }
+    }
+end
+
+function BoolPrompt:onRenderFrame(dc, rect)
+    dialog.MessageBox.super.onRenderFrame(self, dc, rect)
+end
+
 settings_manager = defclass(settings_manager, gui.FramedScreen)
 
 function settings_manager:reset()
     self.frame_title = "Settings"
+    self.file = nil
 end
 
 function settings_manager:init()
@@ -47,7 +90,7 @@ function settings_manager:init()
         text_pen = {fg = COLOR_GREEN},
         cursor_pen = {fg = COLOR_LIGHTGREEN},
         on_submit = self:callback("edit_setting"),
-        frame = {l = 1, t = 1},
+        frame = {l = 1, t = 1, b = 3},
         view_id = "settings_list",
     }
     local settings_page = widgets.Panel{
@@ -72,7 +115,7 @@ function settings_manager:init()
 end
 
 function settings_manager:onInput(keys)
-    page = self.subviews.pages:getSelected()
+    local page = self.subviews.pages:getSelected()
     if keys.LEAVESCREEN then
         if page == 2 then
             self.subviews.pages:setSelected(1)
@@ -85,12 +128,28 @@ function settings_manager:onInput(keys)
 end
 
 function settings_manager:select_file(index, choice)
+    self.file = choice.text:sub(1, choice.text:find('.', 1, true) - 1)
     self.subviews.pages:setSelected(2)
-    self.subviews.settings_list:setChoices({choice.text})
-    self.frame_title = choice.text
+    self.subviews.settings_list:setChoices(self:get_choice_strings(self.file))
+end
+
+function settings_manager:get_choice_strings(file)
+    local settings = SETTINGS[file] or error('Invalid settings file: ' .. file)
+    local choices = {}
+    for i, opt in pairs(settings) do
+        table.insert(choices, ('%-40s %s'):format(opt.desc, opt.type))
+    end
+    return choices
 end
 
 function settings_manager:edit_setting(index, choice)
+    local setting = SETTINGS[self.file][index]
+    if setting.type == 'bool' then
+        BoolPrompt{frame_title=setting.desc}:show()
+    end
+end
+
+function settings_manager:commit_edit(index, value)
     
 end
 
