df0ff2e869fe1625ce63e3de3f226098dd2355f3
lethosor <lethosor@gmail.com>
2014-06-27 17:53:46 -0400

    Add adv-max-skills

---
+++ adv-max-skills.lua
@@ -0,0 +1,8 @@
+-- Sets adventurer skills/attributes to maximum
+if dfhack.gui.getCurFocus() ~= 'setupadventure' then
+    qerror('Must be called on adventure mode setup screen')
+end
+
+adv = dfhack.gui.getCurViewscreen().adventurer
+for k, v in pairs(adv.skills) do adv.skills[k] = 20 end
+for k, v in pairs(adv.attributes) do adv.attributes[k] = 6 end
