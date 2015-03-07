4d6745734ee7fca626c76d933cf86a33103a0970
lethosor <lethosor@gmail.com>
2015-03-07 10:24:25 -0500

    Display keys

--- gui/input-monitor.lua
+++ gui/input-monitor.lua
@@ -23,6 +23,9 @@ end
 viewscreen_inputst = defclass(viewscreen_inputst, gui.Screen)
 viewscreen_inputst.focus_path = 'input_monitor'
 function viewscreen_inputst:init()
+    self.keys = {}
+    self.key_max_length = 0
+    self.key_pressed = false
     self.old_fps = gps.display_frames
     gps.display_frames = 0
 end
@@ -33,7 +36,6 @@ function viewscreen_inputst:onRender()
     p:seek(0, gps.dimy - 1)
     local mx = gps.mouse_x
     local my = gps.mouse_y
-    if my == gps.dimy - 1 then p:seek(nil, 0) end
     p:string(format_modstate(dfhack.internal.getModstate()))
     p:seek(15)
     p:string(('(%i,%i)'):format(mx, my))
@@ -49,12 +51,35 @@ function viewscreen_inputst:onRender()
         p:string('      ')
     end
     p:string(('FPS: %i (%i)'):format(enabler.calculated_fps, enabler.calculated_gfps))
-    p:seek(mx, my):string(' ', {bg = mouse_bg})
+    for y = 0, math.min(#self.keys - 1, gps.dimy - 2) do
+        OutputString(COLOR_GREY, 0, y, self.keys[y + 1])
+    end
+    p:seek(15 + 8 + 6 + 15):string(#self.keys .. ' key' .. (#self.keys == 1 and ' ' or 's'),
+        {fg = self.key_pressed and COLOR_LIGHTGREEN or COLOR_GREY})
+    self.key_pressed = false
+    if mx >= 0 and my >= 0 then
+        local tile = dfhack.screen.readTile(mx, my)
+        tile.bg = mouse_bg
+        dfhack.screen.paintTile(tile, mx, my)
+    end
 end
 
 function viewscreen_inputst:onInput(keys)
     if keys.LEAVESCREEN then
         self:dismiss()
+    else
+        local new_keys = {}
+        self.key_max_length = 0
+        for k, _ in pairs(keys) do
+            if k:sub(1, 1) ~= '_' then
+                table.insert(new_keys, k)
+                self.key_max_length = math.max(self.key_max_length, #k)
+            end
+        end
+        if #new_keys > 0 then
+            self.key_pressed = true
+            self.keys = new_keys
+        end
     end
 end
 
