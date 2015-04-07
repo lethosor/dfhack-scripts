71a59dc8bbebe4f703e0d3d81480c95f1bdb7716
lethosor <lethosor@gmail.com>
2015-04-06 23:13:07 -0400

    Fall back to a Lua penarray implementation

--- gui/manipulator.lua
+++ gui/manipulator.lua
@@ -1,7 +1,26 @@
 -- manipulator
 
-if not dfhack.penarray then
-    qerror('This script requires the dfhack.penarray class (available in 0.40.24-r4 or later)')
+penarray = dfhack.penarray
+if not penarray then
+    print('Falling back to a slower Lua penarray implementation')
+    penarray = {}
+    function penarray.new(width)
+        self = {width = width, grid = {}}
+        for i = 1, width do
+            self.grid[i] = dfhack.pen.parse({})
+        end
+        setmetatable(self, {__index = penarray})
+        return self
+    end
+    function penarray:set_tile(x, y, pen)
+        self.grid[x] = dfhack.pen.parse(pen)
+    end
+    function penarray:draw(x1, y, width, height, bufx, bufy)
+        local x2 = x1 + width - 1
+        for x = x1, x2 do
+            dfhack.screen.paintTile(self.grid[x - x1 + bufx] or {}, x, y)
+        end
+    end
 end
 
 gui = require 'gui'
@@ -489,7 +508,7 @@ function manipulator:init(args)
     self.grid_idx = 1
     self.grid_rows = {}
     for idx, u in pairs(self.units) do
-        self.grid_rows[u] = dfhack.penarray.new(#SKILL_COLUMNS, 1)
+        self.grid_rows[u] = penarray.new(#SKILL_COLUMNS, 1)
         if u._native == args.selected then
             self.list_idx = idx
         end
