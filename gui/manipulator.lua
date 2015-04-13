8e18954950a22aa3ac087cdb19af5c21f7358fd9
lethosor <lethosor@gmail.com>
2015-04-13 17:55:03 -0400

    Allow Lua penarray implementation to be used by specifying a command-line argument.

--- gui/manipulator.lua
+++ gui/manipulator.lua
@@ -1,30 +1,8 @@
 -- manipulator
 
-penarray = dfhack.penarray
-if not penarray then
-    print('Falling back to a slower Lua penarray implementation')
-    penarray = {}
-    function penarray.new(width)
-        self = {width = width, grid = {}}
-        for i = 1, width do
-            self.grid[i] = dfhack.pen.parse({})
-        end
-        setmetatable(self, {__index = penarray})
-        return self
-    end
-    function penarray:set_tile(x, y, pen)
-        self.grid[x] = dfhack.pen.parse(pen)
-    end
-    function penarray:draw(x1, y, width, height, bufx, bufy)
-        local x2 = x1 + width - 1
-        for x = x1, x2 do
-            dfhack.screen.paintTile(self.grid[x - x1 + bufx] or {}, x, y)
-        end
-    end
-end
-
 gui = require 'gui'
 widgets = require 'gui.widgets'
+utils = require 'utils'
 enabler = df.global.enabler
 gps = df.global.gps
 
@@ -210,6 +188,31 @@ SKILL_LEVELS = {
     {name = "Legendary+4",  points = 2400, abbr = 'Y'},
     {name = "Legendary+5",  points = 0,    abbr = 'Z'},
 }
+
+args = {...}
+iargs = utils.invert(args)
+penarray = dfhack.penarray
+if not penarray or iargs['--lua-penarray'] then
+    print('lua penarray')
+    penarray = {}
+    function penarray.new(width)
+        self = {width = width, grid = {}}
+        for i = 1, width do
+            self.grid[i] = dfhack.pen.parse({})
+        end
+        setmetatable(self, {__index = penarray})
+        return self
+    end
+    function penarray:set_tile(x, y, pen)
+        self.grid[x] = dfhack.pen.parse(pen)
+    end
+    function penarray:draw(x1, y, width, height, bufx, bufy)
+        local x2 = x1 + width - 1
+        for x = x1, x2 do
+            dfhack.screen.paintTile(self.grid[x - x1 + bufx] or {}, x, y)
+        end
+    end
+end
 
 function if_nil(v, default)
     if v == nil then return default else return v end
