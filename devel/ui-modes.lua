6379d213760012d2a9e98079103031ef2f803298
lethosor <lethosor@gmail.com>
2015-06-24 01:39:25 -0400

    Add devel/ui-modes.lua

---
+++ devel/ui-modes.lua
@@ -0,0 +1,2 @@
+print('Fortress sidebar: ', df.global.ui.main.mode, df.ui_sidebar_mode[df.global.ui.main.mode])
+print('Adventurer menu: ', df.global.ui_advmode.menu, df.ui_advmode_menu[df.global.ui_advmode.menu])
