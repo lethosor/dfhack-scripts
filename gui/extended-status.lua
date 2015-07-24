4668e5317ee83d126317435fb9729fe89bd6d1d5
lethosor <lethosor@gmail.com>
2015-07-23 20:51:24 -0400

    Add gui/extended-status.lua: Allows for additional z-status pages
    Currently only includes bed/bedroom counts

---
+++ gui/extended-status.lua
@@ -0,0 +1,168 @@
+-- Adds more z-status subpages
+--@ enable = true
+
+gui = require 'gui'
+dialogs = require 'gui.dialogs'
+gps = df.global.gps
+world = df.global.world
+
+if enabled == nil then
+    enabled = false
+end
+
+subpages = {
+    {key = 'CUSTOM_B', desc = 'Bedrooms', screen = 'bedroom_list'},
+}
+
+function derror(msg)
+    dialogs.showMessage('Error', msg, COLOR_LIGHTRED)
+end
+
+status_overlay = defclass(status_overlay, gui.Screen)
+status_overlay.ATTRS = {
+    focus_path = 'status_overlay'
+}
+
+function status_overlay:init()
+    self.show_menu = false
+end
+
+function status_overlay:onRender()
+    local parent = self._native.parent
+    local top_p = gui.Painter.new_xy(1, 1, gps.dimx - 2, 3)
+    top_p:key_pen(COLOR_LIGHTRED)
+    parent:render()
+    if self.show_menu then
+        local p = gui.Painter.new_xy(2, 6, gps.dimx - 3, gps.dimy - 3)
+        p:key_pen(COLOR_LIGHTRED)
+        dfhack.screen.fillRect({}, top_p.x1, top_p.y1, top_p.x2, top_p.y2)
+        dfhack.screen.fillRect({}, p.x1 - 1, p.y1 - 1, p.x2 + 1, p.y2 + 1)
+        top_p:seek(3, 1):string('DFHack status options')
+            :newline(3):key('LEAVESCREEN'):string(': Back')
+        for i, opt in pairs(subpages) do
+            p:key(opt.key):string(': ' .. opt.desc):newline()
+        end
+    else
+        top_p:seek(3, 2):key('CUSTOM_X'):string(': Additional options (DFHack)')
+    end
+end
+
+function status_overlay:onInput(keys)
+    local parent = self._native.parent
+    if self.show_menu then
+        if keys.LEAVESCREEN then
+            self.show_menu = false
+        end
+        for _, opt in pairs(subpages) do
+            if keys[opt.key] then
+                local class = _ENV[opt.screen]
+                if class then
+                    class():show()
+                else
+                    derror('Undefined screen: ' .. opt.screen)
+                end
+                break
+            end
+        end
+    else
+        if keys.CUSTOM_X then
+            self.show_menu = true
+            return
+        end
+        if keys.LEAVESCREEN then
+            self:dismiss()
+        end
+        gui.simulateInput(parent, keys)
+    end
+end
+
+bedroom_list = defclass(bedroom_list, gui.FramedScreen)
+bedroom_list.ATTRS = {
+    focus_path = 'extended_status/bedroom_list',
+    frame_title = 'Bedroom status',
+    frame_inset = 1,
+}
+
+function bedroom_list:init()
+    self.data = {
+        {'Beds', 'beds'},
+        {'Built beds', 'bbeds'},
+        {'Unbuilt beds', 'ubeds'},
+        {'Bedrooms', 'brooms'},
+        {'Owned Bedrooms', 'obrooms'},
+        {'Unowned Bedrooms', 'ubrooms'},
+        {'Units', 'units'},
+        {'Units with bedrooms', 'uwith'},
+        {'Units without bedrooms', 'uwithout'}
+    }
+    for _, d in pairs(self.data) do d.list = {} end
+    local function add(key, item)
+        for _, d in pairs(self.data) do
+            if d[2] == key then
+                table.insert(d.list, item)
+            end
+        end
+    end
+    for _, u in pairs(world.units.active) do
+        if dfhack.units.isCitizen(u) then
+            add('units', u)
+            local has_bed = false
+            for _, b in pairs(u.owned_buildings) do
+                if df.building_bedst:is_instance(b) then
+                    has_bed = true
+                end
+            end
+            if has_bed then
+                add('uwith', u)
+            else
+                add('uwithout', u)
+            end
+        end
+    end
+    for _, bed in pairs(world.items.other.BED) do
+        add('beds', bed)
+        add(bed.flags.in_building and 'bbeds' or 'ubeds', bed)
+    end
+    for _, building in pairs(world.buildings.other.BED) do
+        if building.is_room then
+            add('brooms', building)
+            add(building.owner and 'obrooms' or 'ubrooms', building)
+        end
+    end
+end
+
+function bedroom_list:onRenderBody(p)
+    for _, item in pairs(self.data) do
+        p:string(item[1]):string(': '):string(tostring(#item.list)):newline()
+    end
+end
+
+function bedroom_list:onInput(keys)
+    if keys.LEAVESCREEN then
+        self:dismiss()
+    end
+end
+
+screen_history = screen_history or {'', ''}
+dfhack.onStateChange['gui/extended-status'] = function(e)
+    table.insert(screen_history, dfhack.gui.getCurFocus())
+    table.remove(screen_history, 1)
+    if enabled and screen_history[2] == 'overallstatus' and screen_history[1]:sub(1, 6) ~= 'dfhack' then
+        status_overlay():show()
+    end
+end
+
+args = {...}
+if dfhack_flags.enable then
+    args = {dfhack_flags.enable_state and 'enable' or 'disable'}
+end
+if args[1] == 'enable' then
+    enabled = true
+elseif args[1] == 'disable' then
+    enabled = false
+else
+    print([[Usage:
+    gui/extended-status enable|disable|help
+    enable|disable gui/extended-status
+    ]])
+end
