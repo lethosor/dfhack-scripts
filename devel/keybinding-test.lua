5e80d72e4457c0d6f6a771df338668ccf04a9687
lethosor <lethosor@gmail.com>
2015-08-06 11:03:00 -0400

    color keybinding-test output

--- devel/keybinding-test.lua
+++ devel/keybinding-test.lua
@@ -1,6 +1,8 @@
 args = {...}
 if args[1] == 'print' then
+    dfhack.color(COLOR_LIGHTGREEN)
     print('keybinding invoked: ' .. args[2])
+    dfhack.color()
 elseif args[1] then
     prefix = args[1] .. '-'
 else
