703ba51d70c6e058dff1e5637ed810c4a868811d
lethosor <lethosor@gmail.com>
2015-08-13 14:28:02 -0400

    keybinding-test: Collect and log tests better

--- devel/keybinding-test.lua
+++ devel/keybinding-test.lua
@@ -15,13 +15,16 @@ function letterChars()
     end
 end
 
-function modDesc(n, trailing_dash)
+function modDesc(n, trailing_dash, default)
     local mods = ''
     if bit32.band(n, DFH_MOD_SHIFT) ~= 0 then mods = mods .. 'Shift-' end
     if bit32.band(n, DFH_MOD_ALT) ~= 0   then mods = mods .. 'Alt-'   end
     if bit32.band(n, DFH_MOD_CTRL) ~= 0  then mods = mods .. 'Ctrl-'  end
     if not trailing_dash then
         mods = mods:gsub('-$', '')
+    end
+    if #mods == 0 and default then
+        return tostring(default)
     end
     return mods
 end
@@ -41,50 +44,105 @@ function Scr:next()
         self.want_modstate = math.min(8, self.want_modstate + 1)
         if self.want_modstate >= 8 then
             self.done = true
+            self:export()
             return
         end
     end
     if not self.triggered[self.want_modstate] then
-        self.triggered[self.want_modstate] = {}
+        self.triggered[self.want_modstate] = {count=0}
         for n, ch in letterChars() do
             self.triggered[self.want_modstate][ch] = false
         end
     end
 end
 
+function Scr:countTriggered(key)
+    local t = self.triggered[key]
+    local count = 0
+    t.count = nil
+    for _, v in pairs(t) do
+        if v then
+            count = count + 1
+        end
+    end
+    t.count = count
+end
+
 function Scr:trigger(ch)
     if dfhack.internal.getModstate() ~= self.want_modstate then
-        self.flash_timer = 3
+        self.flash_timer = 4
         return
     end
     self.triggered[self.want_modstate][ch] = true
+    self:countTriggered(self.want_modstate)
     for _, ok in pairs(self.triggered[self.want_modstate]) do
-        if not ok then return end
+        if ok == false then return end
     end
     self:next()
 end
 
+function Scr:export()
+    self.summary = {}
+    for i = 0, 7 do
+        local msg = modDesc(i, false, 'none') .. ': '
+        local failed = {}
+        local color
+        for n, ch in letterChars() do
+            if not self.triggered[i][ch] then
+                table.insert(failed, ch)
+            end
+        end
+        if #failed > 0 then
+            msg = msg .. tostring(#failed) .. ' failed: ' .. table.concat(failed, ', ')
+            color = COLOR_LIGHTRED
+        else
+            msg = msg .. 'all passed'
+            color = COLOR_LIGHTGREEN
+        end
+        table.insert(self.summary, {msg, color})
+        dfhack.color(color)
+        dfhack.print(msg .. '\n')
+        dfhack.color()
+        io.stdout:write(msg .. '\n')
+    end
+    io.stdout:flush()
+end
+
 function Scr:onRenderBody(p)
+    self.flash_timer = math.max(0, (self.flash_timer or 0) - 1)
     dfhack.screen.clear()
     if self.done then
-        p:string('Test complete! Press any key to exit')
+        p:string('Test complete! Press any key to exit'):newline():newline()
+        if self.summary then
+            for _, row in pairs(self.summary) do
+                p:string(table.unpack(row)):newline()
+            end
+            p:newline():string('Also logged to DFHack console and stdout.log')
+        end
         return
     end
-    local desc = self.want_modstate ~= 0 and modDesc(self.want_modstate) or 'no modifiers'
-    p:string(('Press %s (%i/8)'):format(desc, self.want_modstate + 1),
-        dfhack.internal.getModstate() == self.want_modstate and COLOR_LIGHTGREEN or COLOR_LIGHTRED
-    ):newline()
+    p:string('Press ' .. modDesc(self.want_modstate, false, 'no modifiers'),
+        dfhack.internal.getModstate() == self.want_modstate and COLOR_LIGHTGREEN or
+            (self.flash_timer % 2 == 0 and COLOR_LIGHTRED or COLOR_YELLOW)
+    ):newline():string(('group %i/8, %i/26 bindings (%.1f%%)'):format(
+        self.want_modstate + 1,
+        self.triggered[self.want_modstate].count,
+        self.triggered[self.want_modstate].count / 26 * 100
+    )):newline()
     for n, ch in letterChars() do
         p:string(ch, self.triggered[self.want_modstate][ch] and COLOR_LIGHTGREEN or COLOR_DARKGREY):string(' ')
     end
     p:newline():newline()
+    p:key('CURSOR_LEFT'):key('CURSOR_RIGHT'):string(' (cursor left/right): Navigate'):newline()
     p:key('LEAVESCREEN_ALL'):string(': Exit'):newline():newline()
-    p:pen(COLOR_YELLOW)
-    p:string('Warning: Conflicts may occur with macro bindings:'):newline()
-        :key('RECORD_MACRO'):string(', ')
-        :key('PLAY_MACRO'):string(', ')
-        :key('SAVE_MACRO'):string(', ')
-        :key('LOAD_MACRO')
+    if self.want_modstate == DFH_MOD_CTRL then
+        p:pen(COLOR_YELLOW)
+        p:string('Warning: macro bindings may be triggered:'):newline()
+            :key('RECORD_MACRO'):string(', ')
+            :key('PLAY_MACRO'):string(', ')
+            :key('SAVE_MACRO'):string(', ')
+            :key('LOAD_MACRO')
+    end
 end
 
 function Scr:onInput(keys)
