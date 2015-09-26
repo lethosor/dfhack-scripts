31f34c7235b98884dae5bb508baea412cbb7a8f1
lethosor <lethosor@gmail.com>
2015-09-26 13:50:04 -0400

    send-key: allow sending input to parent screens

--- devel/send-key.lua
+++ devel/send-key.lua
@@ -1,4 +1,13 @@
-key = df.interface_key[({...})[1]]
+args = {...}
+key = df.interface_key[args[1]]
 if not key then qerror('Unrecognized key') end
 gui = require 'gui'
-gui.simulateInput(dfhack.gui.getCurViewscreen(), key)
+p = tonumber(args[2])
+scr = dfhack.gui.getCurViewscreen()
+if p ~= nil then
+    while p > 0 do
+        p = p - 1
+        scr = scr.parent
+    end
+end
+gui.simulateInput(scr, key)
