d9fb13c361107123a2a49053fcf29ee1f8a78a8a
lethosor <lethosor@gmail.com>
2017-06-05 20:46:49 -0400

    Add PlantSidebar (incomplete)

--- devel/plant-list.lua
+++ devel/plant-list.lua
@@ -1,4 +1,5 @@
 gui = require 'gui'
+guidm = require 'gui.dwarfmode'
 widgets = require 'gui.widgets'
 
 PlantList = defclass(PlantList, gui.FramedScreen)
@@ -119,7 +120,7 @@ function PlantList:onInput(keys)
     elseif keys.SELECT then
         local plant = dfhack.gui.getSelectedPlant(true)
         if plant then
-            printall(plant.pos)
+            PlantSidebar{plant=plant}:show()
         end
     end
     self:inputToSubviews(keys)
@@ -130,6 +131,67 @@ function PlantList:onGetSelectedPlant()
     if choice then
         return df.global.world.plants.all[choice.index]
     end
+end
+
+
+PlantSidebar = defclass(PlantSidebar, guidm.MenuOverlay)
+
+function PlantSidebar:init(args)
+    PlantSidebar.super.init(self, args)
+    self.plant = args.plant
+    self:addviews{
+        widgets.KeyLabel{
+            key = 'LEAVESCREEN',
+            text = 'Back to list',
+            frame = {l=1, b=2},
+            on_activate = self:callback('dismiss'),
+        },
+        widgets.KeyLabel{
+            key = 'LEAVESCREEN_ALL',
+            text = 'Exit to map',
+            frame = {l=1, b=1},
+            on_activate = self:callback('exitToMap'),
+        },
+    }
+end
+
+function PlantSidebar:onGetSelectedPlant()
+    return self.plant
+end
+
+function PlantSidebar:onAboutToShow()
+    -- skip checks
+    df.global.ui.main.mode = df.ui_sidebar_mode.LookAround
+    dfhack.gui.revealInDwarfmodeMap(self.plant.pos, true)
+    df.global.cursor:assign(xyz2pos(pos2xyz(self.plant.pos)))
+end
+
+function PlantSidebar:sendInputToParent(keys)
+    gui.simulateInput(dfhack.gui.getViewscreenByType(df.viewscreen_dwarfmodest, 0), keys)
+end
+
+function PlantSidebar:renderParent()
+    dfhack.gui.getViewscreenByType(df.viewscreen_dwarfmodest, 0):render()
+end
+
+function PlantSidebar:render(dc)
+    PlantSidebar.super.render(self, dc)
+    local p = gui.Painter.new(self.df_layout.menu)
+    p:seek(1,1):string('Stuff')
+end
+
+function PlantSidebar:onInput(keys)
+    for k in pairs(guidm.MOVEMENT_KEYS) do
+        if keys[k] then
+            self:sendInputToParent(keys)
+            return
+        end
+    end
+    self:inputToSubviews(keys)
+end
+
+function PlantSidebar:exitToMap()
+    dfhack.screen.dismiss(dfhack.gui.getCurViewscreen(), true)
 end
 
 PlantList():show()
