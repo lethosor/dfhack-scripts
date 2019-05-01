fabe7bff89f0efdf7e6b23488297384afc8d55ad
lethosor <lethosor@gmail.com>
2019-04-30 22:39:43 -0400

    Detect win/loss, and auto-mark mines on win

--- gui/minesweeper.lua
+++ gui/minesweeper.lua
@@ -21,6 +21,9 @@ function MSState:init()
         marked = 0,
     }
     self.has_mines = false
+    self.won = false
+    self.lost = false
+    self.safe_cells = self.width * self.height - self.mines
     self.draw_buffer = dfhack.penarray.new(self.width, self.height)
     self:draw()
 end
@@ -88,6 +91,7 @@ function MSState:add_mines(skipx, skipy)
 end
 
 function MSState:reveal(x, y)
+    if self.lost then return end
     if not self.has_mines then
         self:add_mines(x, y)
     end
@@ -97,7 +101,9 @@ function MSState:reveal(x, y)
         if not cell.revealed and not cell.marked then
             cell.revealed = true
             self.counts.revealed = self.counts.revealed + 1
-            if not cell.mine and cell.count == 0 then
+            if cell.mine then
+                self.lost = true
+            elseif cell.count == 0 then
                 for _, neighbor in ipairs(self:cell_neighbors(cell)) do
                     if not neighbor.revealed then
                         table.insert(queue, neighbor)
@@ -106,13 +112,42 @@ function MSState:reveal(x, y)
             end
         end
     end
+    if self.counts.revealed == self.safe_cells then
+        self:mark_all()
+        self.won = true
+    end
+end
+
+function MSState:reveal_all()
+    local dimx, dimy = self:grid_dims()
+    for x = 1, dimx do
+        for y = 1, dimy do
+            if not self.grid[x][y].revealed then
+                self:reveal(x, y)
+            end
+        end
+    end
 end
 
 function MSState:mark(x, y)
+    if self.lost then return end
     local cell = self.grid[x][y]
     if not cell.revealed then
         cell.marked = not cell.marked
         self.counts.marked = self.counts.marked + (cell.marked and 1 or -1)
+    end
+end
+
+function MSState:mark_all()
+    local dimx, dimy = self:grid_dims()
+    for x = 1, dimx do
+        for y = 1, dimy do
+            local cell = self.grid[x][y]
+            if cell.mine and not cell.revealed and not cell.marked then
+                cell.marked = true
+                self.counts.marked = self.counts.marked + 1
+            end
+        end
     end
 end
 
@@ -139,10 +174,14 @@ MSScreen = defclass(MSScreen, gui.FramedScreen)
 
 function MSScreen:init()
     if not MSState.instance then
-        MSState.instance = MSState()
+        self:new_game()
     end
     local dimx, dimy = MSState.instance:grid_dims()
     self.cursor = {x = math.floor(dimx / 2), y = math.floor(dimy / 2)}
+end
+
+function MSScreen:new_game()
+    MSState.instance = MSState({width=20})
 end
 
 function MSScreen:onRenderBody(p)
@@ -155,6 +194,18 @@ function MSScreen:onRenderBody(p)
 
     local gridx, gridy = state:grid_dims()
     local sidebar = gui.Painter.new_xy(60, p.y1 + 1, p.x2 - 1, p.y2 - 1)
+    local dwarf_pen = {fg = COLOR_GREY}
+    local status = ''
+    if state.won then
+        status = 'You win!'
+        if math.floor(dfhack.getTickCount() / 333) % 3 == 0 then
+            dwarf_pen.fg = COLOR_WHITE
+        end
+    elseif state.lost then
+        dwarf_pen.bg = COLOR_RED
+        status = 'You died.'
+    end
+    sidebar:char(1, dwarf_pen):string(' ' .. status):newline()
     sidebar:string(('Mines: %d/%d'):format(state.counts.marked, state.mines)):newline()
     sidebar:string(('Revealed: %d/%d'):format(state.counts.revealed, gridx * gridy - state.mines)):newline()
     sidebar:newline()
@@ -172,15 +223,16 @@ function MSScreen:onInput(keys)
     elseif keys.CUSTOM_X or keys.CUSTOM_M then
         state:mark(self.cursor.x, self.cursor.y)
     elseif keys.CUSTOM_SHIFT_D then
-        for x = 1, gridx do
-            for y = 1, gridy do
-                state:reveal(x, y)
-            end
-        end
+        state:reveal_all()
     elseif keys.CUSTOM_SHIFT_N then
-        dlg.showYesNoPrompt('New game', 'Are you sure you want to start a new game? All progress will be lost.', COLOR_LIGHTRED, function()
-            MSState.instance = MSState()
-        end)
+        if state.won or state.lost then
+            self:new_game()
+        else
+            dlg.showYesNoPrompt('New game',
+                'Are you sure you want to start a new game? All progress will be lost.',
+                COLOR_LIGHTRED,
+                self:callback('new_game'))
+        end
     end
 
     for k, _ in pairs(keys) do
